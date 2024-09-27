4fcb9100ae636777800dbd463697af6374c2add2
cptsalt <72434260+cptsalt@users.noreply.github.com>
2024-09-27 17:30:41 -0600

    Update sh_playsoundpackage.lua
    Updated license

--- sh_playsoundpackage.lua
+++ sh_playsoundpackage.lua
@@ -3,12 +3,7 @@ local PLUGIN = PLUGIN
 PLUGIN.name = "PlaySound Package"
 PLUGIN.author = "salt"
 PLUGIN.description = "Adds commands to play global/local sounds from a file, a global URL stream, and an entity mounted local sound. Also adds a global stopsound."
-PLUGIN.license = [[
-Copyright 2021 Captain Salt
-Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
-The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-]]
+PLUGIN.license = [[ Previous MIT License is void, see the ixplugins-public LICENSE file for the current license (MPL2.0) or visit https://www.mozilla.org/en-US/MPL/2.0/]]
 
 if(SERVER)then
   util.AddNetworkString("SoundGlobal")
